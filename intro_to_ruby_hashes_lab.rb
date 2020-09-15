0f521ed31b1a87cc0cb9fb38aefebc781a911df9
Jacob Kagon <jacob@kagon.net>
2020-09-15 23:42:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,14 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  
+  bash_hash = {
+   :railroads => {
+    :pieces => 4,
+    :rent_in_dollars => {},
+    :names => {}
+   }
+    
+  }
 
 end
 
